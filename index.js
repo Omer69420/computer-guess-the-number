b8700ae8746abd649b1c221f29eed0ce7e535870
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-09 15:58:12 +0100

    WIP steg 1.4

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ let guess
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
-
+    setMessage('för högt')
 })
+
+document.getElementById('Is-correcto-btn').addEventListener('click', function() {
+setMessage('Wohoo!')
+})
+
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
-    
+    setMessage('för lågt')
 })
+
 
 
 function setMessage(msg) {
