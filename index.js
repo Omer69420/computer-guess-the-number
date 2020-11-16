93514f876710af3277c17dbe5cd65fe329f9b384
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-16 15:48:19 +0100

    hittils klar med 3

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
-let guess
+let newMaxNumber = 
+
+let guess = 50
+
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
     setMessage('för högt')
+   // guess--
+   
+    setMessage(`Is it ${guess}?`)
 })
 
 document.getElementById('Is-correcto-btn').addEventListener('click', function() {
-setMessage('Wohoo!')
+    setMessage('Wohoo!')
 })
 
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
     setMessage('för lågt')
+   // guess++
+   guess = radom(MAX_NUMBER)
+    setMessage(`Is it ${guess}?`)
 })
 
 
@@ -28,10 +37,9 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
-
-    // setMessage(`Is it ${guess}?`)
-}
+    guess = Math.floor(Math.random() * 101)
+     setMessage(`Is it ${guess}?`)
+    }
 
 document
     .getElementById('start-btn')
