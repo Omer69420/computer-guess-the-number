4884bbc18c34f43441d06dfbbd86919be3428a8f
Ömer Cinar <omer.cinar@skola.botkyrka.se>
2020-11-22 13:21:19 +0100

    Klarrr

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const MAX_NUMBER = 100
 const MIN_NUMBER = 0
 
-let newMaxNumber = 
+let guessCount = 0
 
-let guess = 50
+let newMaxNumber = 100
 
+let newLowNumber = 0
+
+let guess
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
-    setMessage('för högt')
-   // guess--
-   
+    // makeGuess()
+    // guess--
+    newMaxNumber = guess
+    guess = lowMiddleNumber(newLowNumber, guess)
     setMessage(`Is it ${guess}?`)
+    setCounter()
 })
 
-document.getElementById('Is-correcto-btn').addEventListener('click', function() {
-    setMessage('Wohoo!')
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    setMessage("Woho!")
 })
-
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
-    setMessage('för lågt')
-   // guess++
-   guess = radom(MAX_NUMBER)
+    // makeGuess()
+    // guess++
+    newLowNumber = guess
+    guess = highMiddleNumber(newMaxNumber, guess)    //Medelvärde
     setMessage(`Is it ${guess}?`)
+    setCounter()
 })
-
 
 
 function setMessage(msg) {
@@ -36,12 +41,30 @@ function setMessage(msg) {
 function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
+    
+    makeGuess()    
 
-    guess = Math.floor(Math.random() * 101)
-     setMessage(`Is it ${guess}?`)
-    }
+    setMessage(`Is it ${guess}?`)
+}
+globalThis.setMessage = setMessage;
 
 document
     .getElementById('start-btn')
     .addEventListener('click', start)
 
+function makeGuess(){
+    guess = Math.floor(Math.random() * 101)
+
+}
+
+function highMiddleNumber(max, highGuess){
+    highGuess = (max + guess)/2
+    console.log(guess)
+    return Math.round(highGuess)
+}
+
+function lowMiddleNumber(min, lowGuess){
+    lowGuess = (min + guess)/2
+    console.log(guess)
+    return Math.round(lowGuess)
+}
